a8afde804e60b1111c8f415d92a1407bab885ba6
ka12x123 <leedg9107@gmail.com>
2023-12-04 06:56:01 +0900

    g

--- bin/www.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const PORT = 3000;
-const app = require("../app"); //이르케 할 때마다 해당 파일에다가 module.exports = app; 이런거 해줘야함 
-app.listen(PORT, () => {
-    console.log("서버 가동");
-});
-
-//bin   바이너리라는 뜻
